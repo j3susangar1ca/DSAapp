4ca771f4ed6ab61ce67b23e3918724e68d0bc523
j3susangar1ca <j3susangar1ca@gmail.com>
2026-03-05 23:01:56 -0600

    G

--- DSA.Application/Interfaces/IScannerService.cs
+++ DSA.Application/Interfaces/IScannerService.cs
@@ -1,10 +1,15 @@
 namespace DSA.Application.Interfaces;
 
+using System;
+using System.Threading.Tasks;
+
 /// <summary>
 /// Abstracción para la integración de bajo nivel con hardware TWAIN/WIA.
 /// </summary>
 public interface IScannerService
 {
+    bool IsDeviceBusy { get; }
+
     // Asincronía obligatoria para no bloquear el UI Thread (WinUI 3)
     Task<byte[]> CaptureAsync(Guid documentoId);
 }
